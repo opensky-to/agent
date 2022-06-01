adcdf4b1a2b20a91912421da2dc571b0f6824580
sushi.at <sushi@opensky.to>
2022-06-02 00:05:37 +0100

    Added support for "sliding" CG forward/aft

--- OpenSky.Agent/Views/Models/FlightTrackingViewModel.PayloadStations.cs
+++ OpenSky.Agent/Views/Models/FlightTrackingViewModel.PayloadStations.cs
@@ -7,6 +7,7 @@
 namespace OpenSky.Agent.Views.Models
 {
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Diagnostics;
 
@@ -32,6 +33,35 @@ namespace OpenSky.Agent.Views.Models
         /// </summary>
         /// -------------------------------------------------------------------------------------------------
         private bool isPayloadExpanded;
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// The payload forward/aft distribution slider value.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        private double payloadForwardAft = 5;
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the payload forward/aft distribution slider value.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public double PayloadForwardAft
+        {
+            get => this.payloadForwardAft;
+        
+            set
+            {
+                if(Equals(this.payloadForwardAft, value))
+                {
+                   return;
+                }
+        
+                this.payloadForwardAft = value;
+                this.NotifyPropertyChanged();
+                this.SuggestPayloadCommand.DoExecute(null);
+            }
+        }
 
         /// -------------------------------------------------------------------------------------------------
         /// <summary>
@@ -126,6 +156,8 @@ namespace OpenSky.Agent.Views.Models
             }
         }
 
+        private double[] sliderPositionStarts = new[] { 2, 1.8, 1.6, 1.4, 1.2, 1, 0.8, 0.6, 0.4, 0.2, 0 };
+
         /// -------------------------------------------------------------------------------------------------
         /// <summary>
         /// Suggest payload loading.
@@ -155,20 +187,34 @@ namespace OpenSky.Agent.Views.Models
                 }
             }
 
-            // Distribute the rest evenly
+            // Distribute the rest evenly, taking the CG slider value into consideration
+            var nonPilotOverride = false;
             if (nonPilotStations == 0)
             {
                 nonPilotStations = this.Simulator.PayloadStations.Count;
+                nonPilotOverride = true;
             }
 
             if (nonPilotStations > 0)
             {
                 var payloadShare = payloadToLoad / nonPilotStations;
+                var payloadMultipliers = new List<double>();
+
+                var startValue = 2 - (this.PayloadForwardAft * 0.2);
+                var endValue = 2 - startValue;
+                var step = (endValue - startValue) / (nonPilotStations - 1);
+                for (var i = 0; i < nonPilotStations; i++)
+                {
+                    payloadMultipliers.Add(startValue);
+                    startValue += step;
+                }
+
+                var nonPilotIndex = 0;
                 for (var i = 0; i < this.Simulator.PayloadStations.Count; i++)
                 {
-                    if (!this.Simulator.PayloadStations.Names[i].Contains("PILOT"))
+                    if (!this.Simulator.PayloadStations.Names[i].Contains("PILOT") || nonPilotOverride)
                     {
-                        this.PayloadStationWeights[i] = payloadShare;
+                        this.PayloadStationWeights[i] = payloadShare * payloadMultipliers[nonPilotIndex++];
                         payloadToLoad -= payloadShare;
                     }
                 }
